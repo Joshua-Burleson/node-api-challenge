c878d4687220993e586b23e71c08305b7f6bf3d8
Joshua Burleson <josh.burleson@bytes.co>
2020-02-21 21:55:34 -0500

    STRETCH mostly completed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const actionRouter = require('./routes/actionRouter');
 const projectRouter = require('./routes/projectRouter');
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/actions', actionRouter);
 app.use('/api/projects', projectRouter);
 
